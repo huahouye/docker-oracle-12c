0486a8809faa5bbbd5f85b9dd7044a5b91f428fc
huahouye <huahouye@DESKTOP-957QMJA>
2017-07-21 20:16:36 +0800

    修改脚本

--- install/install.sh
+++ install/install.sh
@@ -1,54 +1,26 @@
 #!/bin/sh
 
-# install oracle 12c
-# reference https://wiki.centos.org/HowTos/Oracle12onCentos7
-# https://pierreforstmanndotcom.wordpress.com/2013/06/27/how-to-install-oracle-12-1-in-silent-mode/
-# http://dbaora.com/install-oracle-in-silent-mode-11g-release-2-11-2/
-# https://pierreforstmanndotcom.wordpress.com/2017/03/01/oracle-database-12-2-0-1-silent-installation-and-database-creation-on-oracle-linux-7/
+# install oracle 12c release 2
+
 # http://dbaora.com/install-oracle-in-silent-mode-12c-release-2-12-2-on-oel7/
 
 set -x
 
-groupadd oinstall
-groupadd dba
-useradd -g oinstall -G dba oracle
-echo oracle | passwd oracle --stdin
+echo "127.0.0.1 oel7 oel7.dbaora.com localhost.localdomain localhost" >> /etc/hosts
+hostnamectl set-hostname oel7.dbaora.com --static
 
-cat << EOF >> /etc/sysctl.conf
-fs.aio-max-nr = 1048576
-fs.file-max = 6815744
-kernel.shmall = 2097152
-kernel.shmmax = 1987162112
-kernel.shmmni = 4096
-kernel.sem = 250 32000 100 128
-net.ipv4.ip_local_port_range = 9000 65500
-net.core.rmem_default = 262144
-net.core.rmem_max = 4194304
-net.core.wmem_default = 262144
-net.core.wmem_max = 1048586
-EOF
+groupadd -g 54321 oinstall
+groupadd -g 54322 dba
+groupadd -g 54323 oper
+groupadd -g 54324 backupdba
+groupadd -g 54325 dgdba
+groupadd -g 54326 kmdba
+groupadd -g 54327 asmdba
+groupadd -g 54328 asmoper
+groupadd -g 54329 asmadmin
+groupadd -g 54330 racdba
 
-cat << EOF >> /etc/security/limits.conf
-oracle soft nproc 2047
-oracle hard nproc 16384
-oracle soft nofile 1024
-oracle hard nofile 65536
-EOF
-
-mkdir /u01
-mkdir /u01/db12201
-mkdir /u01/orainv
-mkdir /u01/oracle
-chmod g+s /u01
-chown -R oracle:dba /u01
-mkdir /u02
-mkdir /u02/db12201
-mkdir /u02/orainv
-mkdir /u02/oracle
-chmod g+s /u02
-chown -R oracle:dba /u02
-
-chown -R oracle:oinstall /tmp/
+useradd -u 54321 -g oinstall -G dba,oper,backupdba,dgdba,kmdba,racdba oracle
 
 yum install -y binutils.x86_64 compat-libcap1.x86_64 gcc.x86_64 gcc-c++.x86_64 glibc.i686 glibc.x86_64 \
 glibc-devel.i686 glibc-devel.x86_64 ksh compat-libstdc++-33 libaio.i686 libaio.x86_64 libaio-devel.i686 libaio-devel.x86_64 \
@@ -56,13 +28,199 @@ libgcc.i686 libgcc.x86_64 libstdc++.i686 libstdc++.x86_64 libstdc++-devel.i686 l
 libXtst.i686 libXtst.x86_64 make.x86_64 sysstat.x86_64
 ##### yum clean all
 
-rm -rf /u01/orainv/logs
-su - oracle -c "/install-su-oracle.sh"
+cat << EOF >> /etc/sysctl.conf
+fs.file-max = 6815744
+kernel.sem = 250 32000 100 128
+kernel.shmmni = 4096
+kernel.shmall = 1073741824
+kernel.shmmax = 4398046511104
+net.core.rmem_default = 262144
+net.core.rmem_max = 4194304
+net.core.wmem_default = 262144
+net.core.wmem_max = 1048576
+fs.aio-max-nr = 1048576
+net.ipv4.ip_local_port_range = 9000 65500
+kernel.panic_on_oops=1
+EOF
 
-/u01/orainv/orainstRoot.sh
-/u01/db12201/root.sh
-cat /u01/db12201/install/root_ol7ttfs1.localdomain_2017-03-01_21-09-04-788037704.log
+/sbin/sysctl -p
 
-export ORACLE_HOME=/u01/db12201
-PATH=$ORACLE_HOME/bin:$PATH
-$ORACLE_HOME/OPatch/opatch lsinv
+cat << EOF >> /etc/security/limits.conf
+oracle   soft   nofile   1024
+oracle   hard   nofile   65536
+oracle   soft   nproc    2047
+oracle   hard   nproc    16384
+oracle   soft   stack    10240
+oracle   hard   stack    32768
+oracle   soft   memlock  3145728
+oracle   hard   memlock  3145728
+EOF
+
+systemctl stop firewalld
+systemctl disable firewalld
+
+cat << \EOF >> /home/oracle/.bash_profile
+export TMP=/tmp
+
+export ORACLE_HOSTNAME=oel7.dbaora.com
+export ORACLE_UNQNAME=ORA12C
+export ORACLE_BASE=/ora01/app/oracle
+export ORACLE_HOME=$ORACLE_BASE/product/12.2.0/db_1
+export ORACLE_SID=ORA12C
+
+PATH=/usr/sbin:$PATH:$ORACLE_HOME/bin
+
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib;
+export CLASSPATH=$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib;
+
+export PATH
+
+alias cdob='cd $ORACLE_BASE'
+alias cdoh='cd $ORACLE_HOME'
+alias tns='cd $ORACLE_HOME/network/admin'
+alias envo='env | grep ORACLE'
+
+umask 022
+
+if [ $USER = "oracle" ]; then
+    if [ $SHELL = "/bin/ksh" ]; then
+       ulimit -u 16384 
+       ulimit -n 65536
+    else
+       ulimit -u 16384 -n 65536
+    fi
+fi
+
+envo
+EOF
+
+mkdir -p /ora01/app/oracle/product/12.2.0/db_1
+chown oracle:oinstall -R /ora01
+
+## systemctl mask tmp.mount
+chown oracle:oinstall -R /tmp/database
+
+# edit oracle rsp files
+cat << \EOF > /tmp/database/response/db_install.rsp
+oracle.install.responseFileVersion=/oracle/install/rspfmt_dbinstall_response_schema_v12.2.0
+oracle.install.option=INSTALL_DB_SWONLY
+UNIX_GROUP_NAME=oinstall
+INVENTORY_LOCATION=/ora01/app/oraInventory
+ORACLE_HOME=/ora01/app/oracle/product/12.2.0/db_1
+ORACLE_BASE=/ora01/app/oracle
+oracle.install.db.InstallEdition=EE
+oracle.install.db.OSDBA_GROUP=dba
+oracle.install.db.OSOPER_GROUP=oper
+oracle.install.db.OSBACKUPDBA_GROUP=backupdba
+oracle.install.db.OSDGDBA_GROUP=dgdba
+oracle.install.db.OSKMDBA_GROUP=kmdba
+oracle.install.db.OSRACDBA_GROUP=racdba
+oracle.install.db.rac.configurationType=
+oracle.install.db.CLUSTER_NODES=
+oracle.install.db.isRACOneInstall=
+oracle.install.db.racOneServiceName=
+oracle.install.db.rac.serverpoolName=
+oracle.install.db.rac.serverpoolCardinality=
+oracle.install.db.config.starterdb.type=
+oracle.install.db.config.starterdb.globalDBName=
+oracle.install.db.config.starterdb.SID=
+oracle.install.db.ConfigureAsContainerDB=
+oracle.install.db.config.PDBName=
+oracle.install.db.config.starterdb.characterSet=
+oracle.install.db.config.starterdb.memoryOption=
+oracle.install.db.config.starterdb.memoryLimit=
+oracle.install.db.config.starterdb.installExampleSchemas=
+oracle.install.db.config.starterdb.password.ALL=
+oracle.install.db.config.starterdb.password.SYS=
+oracle.install.db.config.starterdb.password.SYSTEM=
+oracle.install.db.config.starterdb.password.DBSNMP=
+oracle.install.db.config.starterdb.password.PDBADMIN=
+oracle.install.db.config.starterdb.managementOption=
+oracle.install.db.config.starterdb.omsHost=
+oracle.install.db.config.starterdb.omsPort=
+oracle.install.db.config.starterdb.emAdminUser=
+oracle.install.db.config.starterdb.emAdminPassword=
+oracle.install.db.config.starterdb.enableRecovery=
+oracle.install.db.config.starterdb.storageType=
+oracle.install.db.config.starterdb.fileSystemStorage.dataLocation=
+oracle.install.db.config.starterdb.fileSystemStorage.recoveryLocation=
+oracle.install.db.config.asm.diskGroup=
+oracle.install.db.config.asm.ASMSNMPPassword=
+MYORACLESUPPORT_USERNAME=
+MYORACLESUPPORT_PASSWORD=
+SECURITY_UPDATES_VIA_MYORACLESUPPORT=
+DECLINE_SECURITY_UPDATES=
+PROXY_HOST=
+PROXY_PORT=
+PROXY_USER=
+PROXY_PWD=
+COLLECTOR_SUPPORTHUB_URL=
+EOF
+
+cat << \EOF > /tmp/database/response/dbca.rsp
+gdbName=ORA12C.dbaora.com
+sid=ORA12C
+createAsContainerDatabase=true
+numberOfPDBs=1
+pdbName=PORA12C1
+pdbAdminPassword=Oracle12c
+templateName=General_Purpose.dbc
+sysPassword=Oracle12c
+systemPassword=Oracle12c
+emConfiguration=DBEXPRESS
+emExpressPort=5500
+dbsnmpPassword=Oracle12c
+datafileDestination=/ora01/app/oracle/oradata
+recoveryAreaDestination=/ora01/app/oracle/flash_recovery_area
+storageType=FS
+characterSet=AL32UTF8
+nationalCharacterSet=AL16UTF16
+listeners=LISTENER
+sampleSchema=true
+databaseType=OLTP
+automaticMemoryManagement=TRUE
+totalMemory=450
+EOF
+
+## Start binaries installation (run as oracle)
+### wait for several minutes
+su - oracle -c "cd /tmp/database; ./runInstaller -silent -responseFile /tmp/database/response/db_install.rsp"
+### sqlplus / as sysdba
+
+/ora01/app/oraInventory/orainstRoot.sh
+/ora01/app/oracle/product/12.2.0/db_1/root.sh
+
+## Configure Oracle Net
+### 端口 1521 被占用，实际没有 http://blog.csdn.net/xiangsir/article/details/8632048
+### less /ora01/app/oracle/cfgtoollogs/netca/trace_OraDB12Home1-1707215PM0219.log
+su - oracle -c "netca -silent -responseFile /tmp/database/response/netca.rsp"
+
+## run database installation (run as oracle)
+su - oracle -c "lsnrctl start && lsnrctl status && mkdir /ora01/app/oracle/oradata && mkdir /ora01/app/oracle/flash_recovery_area"
+### less /ora01/app/oracle/cfgtoollogs/dbca/ORA12C/ORA12C0.log
+su - oracle -c "dbca -silent -createDatabase -responseFile /tmp/database/response/dbca.rsp"
+### sqlplus / as sysdba
+### SQL> startup
+### SQL> show parameter db_name
+### NAME               TYPE     VALUE
+### ----------- ----------- -------------
+### db_name          string     ORA12C
+### SQL> alter session set container=PORA12C1;
+### Session altered.
+### SQL> show con_id
+### CON_ID
+### ------------------------------
+### 3
+### SQL> show con_name
+### CON_NAME
+### ------------------------------
+### PORA12C1
+### SQL>
+
+## vi /etc/oratab
+### ORA12C:/ora01/app/oracle/product/12..0/db_1:Y
+
+
+
+
+
